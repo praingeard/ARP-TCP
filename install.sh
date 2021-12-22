fb3a145ccdef275aeb622a2a9315a943b80d96b7
root <p.raingeard.37@gmail.com>
2021-12-22 17:29:55 +0000

    changed unnamed pipes for large amont of data

--- install.sh
+++ install.sh
@@ -1,9 +1,20 @@
-unzip -u archive.zip 
-cd archive
+pwd=$(pwd)
+if [ -z "$1" ]; then 
+    ARCHIVEVAR=~/archive
+else 
+    ARCHIVEVAR=$1
+fi
+
+unzip -u -d /tmp/archive archive.zip
+rm -rf $ARCHIVEVAR
+mkdir $ARCHIVEVAR
+mv  -v /tmp/archive/archive/* $ARCHIVEVAR
+cd $ARCHIVEVAR
 make 
-make socket/client
-make master/master
-make socket/server
-make unnamed_pipe/unnamed_pipe
 make named_pipe/named_pipe
 make circle_buffer/circle_buffer
+make unnamed_pipe/unnamed_pipe
+make socket/client
+make socket/server
+make master/master
+cd $pwd
