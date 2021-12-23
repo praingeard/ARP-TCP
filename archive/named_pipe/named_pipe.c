149a62c57453586736c251c289460855564deea1
root <p.raingeard.37@gmail.com>
2021-12-23 09:55:01 +0000

    pipes working for large data

--- archive/named_pipe/named_pipe.c
+++ archive/named_pipe/named_pipe.c
@@ -8,41 +8,49 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <errno.h>
+#include <time.h>
 
 #define FIFO_NAME "/tmp/named_fifo"
 const mode_t FIFO_MODE = 0660;
 
-void *job(int length_msg)
+void *job(int number_sends,int last_send, int length_msg)
 {
 	int fdread;
-	int LENGTH_MSG = length_msg;
 	if ((fdread = open(FIFO_NAME, O_RDONLY)) == -1)
 	{
 		fprintf(stderr, "Impossible to open the pipe: %s\n",
 				strerror(errno));
 		exit(EXIT_FAILURE);
 	}
-	char buffer[LENGTH_MSG];
-	read(fdread, buffer, LENGTH_MSG);
-	printf("The message is : %s", buffer);
-	pthread_exit(EXIT_SUCCESS);
+	char buffer[length_msg];
+	char buffer_last[last_send];
+	if (number_sends != 0)
+        {
+            for (int i = 0; i < number_sends; i++)
+            {
+                // lecture dans le tube
+               read(fdread, buffer, length_msg);
+            printf("data %s\n", buffer);
+            }
+        }
+        if (last_send!= 0){
+       	read(fdread, buffer_last, last_send);
+            printf("last data %s\n", buffer_last);
+        }
 }
 
 int main(int argc, char *argv[])
 {
 	int LENGTH_MSG = atoi(argv[1]);
-	char message[LENGTH_MSG];
-	int i, n, rnd;
-	srand(time(NULL));
-	for (i = 0; i < LENGTH_MSG - 1; ++i)
-	{
-		rnd = rand();
-		n = (rnd >> 4) & 0xF;
-		*(message + i) = (rnd & 0xF) & 1
-							 ? (n % 10) + '0'
-							 : (n % 26) + 'A';
-	}
-	message[LENGTH_MSG - 1] = 0;
+    int number_of_sends = 0;
+    int last_send = LENGTH_MSG;
+    if (LENGTH_MSG > 10000)
+    {
+        number_of_sends = LENGTH_MSG / 10000;
+        last_send = LENGTH_MSG % 10000;
+        LENGTH_MSG = 10000;
+        //printf("%i, %i, %i \n", number_of_sends, last_send, LENGTH_MSG);
+    }
 	if (mkfifo(FIFO_NAME, FIFO_MODE) == -1)
 	{
 		printf("Destroying already existing fifo: %s\n", FIFO_NAME);
@@ -61,7 +69,7 @@ int main(int argc, char *argv[])
 	}
 	if (child == 0)
 	{
-		job(LENGTH_MSG);
+		job(number_of_sends,last_send, LENGTH_MSG);
 	}
 	else
 	{
@@ -72,7 +80,48 @@ int main(int argc, char *argv[])
 				   strerror(errno));
 			exit(EXIT_FAILURE);
 		}
-		write(fdwrite, message, sizeof(message));
+		 char* message = NULL;
+        char* last_message = NULL;
+        int i, n, rnd;
+		if(number_of_sends != 0){
+        for (int j = 0; j < number_of_sends; j++)
+        {
+            message = malloc(LENGTH_MSG);
+            srand(time(NULL));
+            if (number_of_sends != 0)
+            {
+
+                for (i = 0; i < LENGTH_MSG - 1; ++i)
+                {
+                    rnd = rand();
+                    n = (rnd >> 4) & 0xF;
+                    *(message + i) = (rnd & 0xF) & 1
+                                            ? (n % 10) + '0'
+                                            : (n % 26) + 'A';
+                }
+                message[LENGTH_MSG - 1] = 0;
+            }
+
+            write(fdwrite, message, LENGTH_MSG);
+            usleep(20000);
+            free(message);
+        }
+		}
+        if (last_send != 0){
+        last_message = malloc(last_send);
+        for (i = 0; i < last_send - 1; ++i)
+        {
+            rnd = rand();
+            n = (rnd >> 4) & 0xF;
+            *(last_message + i) = (rnd & 0xF) & 1
+                                      ? (n % 10) + '0'
+                                      : (n % 26) + 'A';
+        }
+        last_message[last_send - 1] = 0;
+		write(fdwrite, last_message, last_send);
+        free(last_message);
+        }
+		
 		return EXIT_SUCCESS;
 	}
 }
--- archive/unnamed_pipe/unnamed_pipe.c
+++ archive/unnamed_pipe/unnamed_pipe.c
@@ -4,30 +4,34 @@
 #include <fcntl.h>
 #include <time.h>
 
-void job(int *tube,int number_sends, int last_send, int LENGTH_MSG)
+void job(int *tube, int number_sends, int last_send, int LENGTH_MSG)
 {
-    char message[LENGTH_MSG]; 
-    char last_message[last_send]; 
+    char message[LENGTH_MSG];
+    char last_message[last_send];
     int tid = getpid();
     // timer pour attendre maximum 5 secondes
     int i = 5;
     while (i > 0)
     {
-        if(number_sends != 0){
-        for(int i=0; i < number_sends; i++){
-            // lecture dans le tube
-            if (read(*tube, message, LENGTH_MSG) > 0)
+        if (number_sends != 0)
+        {
+            for (int i = 0; i < number_sends; i++)
             {
-                printf("data %s\n", message);
+                // lecture dans le tube
+                if (read(*tube, message, LENGTH_MSG) > 0)
+                {
+                    printf("data %s\n", message);
+                }
+                usleep(100);
             }
-            usleep(100);
         }
-        }
+        if (last_send!= 0){
         if (read(*tube, last_message, last_send) > 0)
-            {
-                printf("last data %s\n", last_message);
-                break;
-            }
+        {
+            printf("last data %s\n", last_message);
+            break;
+        }
+        }
         //sleep(1);
     }
 }
@@ -37,38 +41,13 @@ int main(int argc, char *argv[])
     int LENGTH_MSG = atoi(argv[1]);
     int number_of_sends = 0;
     int last_send = LENGTH_MSG;
-    if (LENGTH_MSG > 10000){
-        number_of_sends = LENGTH_MSG/10000;
-        last_send = LENGTH_MSG%10000;
+    if (LENGTH_MSG > 10000)
+    {
+        number_of_sends = LENGTH_MSG / 10000;
+        last_send = LENGTH_MSG % 10000;
         LENGTH_MSG = 10000;
         //printf("%i, %i, %i \n", number_of_sends, last_send, LENGTH_MSG);
     }
-    char message[number_of_sends][LENGTH_MSG];
-    char last_message[last_send];
-    int i, n, rnd;
-    srand(time(NULL));
-    if (number_of_sends != 0){
-    for (int j = 0; j < number_of_sends; j++){
-   
-    
-        for (i = 0; i < LENGTH_MSG - 1; ++i){
-            rnd = rand();
-            n = (rnd >> 4) & 0xF;
-            *(message[j] + i) = (rnd & 0xF) & 1
-                             ? (n % 10) + '0'
-                             : (n % 26) + 'A';
-        }
-    message[j][LENGTH_MSG - 1] = 0;
-    }
-    }
-    for (i = 0; i < last_send - 1; ++i){
-            rnd = rand();
-            n = (rnd >> 4) & 0xF;
-            *(last_message + i) = (rnd & 0xF) & 1
-                             ? (n % 10) + '0'
-                             : (n % 26) + 'A';
-    }
-    last_message[last_send - 1] = 0;
     int tube[2];
     pipe(tube);
     pid_t pid = fork();
@@ -86,11 +65,45 @@ int main(int argc, char *argv[])
     }
     else
     {
-        for (int j = 0; j < number_of_sends; j++){
-        write(tube[1], message[j], LENGTH_MSG);
-        usleep(5000);
+        char* message = NULL;
+        char* last_message = NULL;
+        int i, n, rnd;
+        for (int j = 0; j < number_of_sends; j++)
+        {
+            message = malloc(LENGTH_MSG);
+            srand(time(NULL));
+            if (number_of_sends != 0)
+            {
+
+                for (i = 0; i < LENGTH_MSG - 1; ++i)
+                {
+                    rnd = rand();
+                    n = (rnd >> 4) & 0xF;
+                    *(message + i) = (rnd & 0xF) & 1
+                                            ? (n % 10) + '0'
+                                            : (n % 26) + 'A';
+                }
+                message[LENGTH_MSG - 1] = 0;
+            }
+
+            write(tube[1], message, LENGTH_MSG);
+            usleep(10000);
+            free(message);
         }
+        if (last_send != 0){
+        last_message = malloc(last_send);
+        for (i = 0; i < last_send - 1; ++i)
+        {
+            rnd = rand();
+            n = (rnd >> 4) & 0xF;
+            *(last_message + i) = (rnd & 0xF) & 1
+                                      ? (n % 10) + '0'
+                                      : (n % 26) + 'A';
+        }
+        last_message[last_send - 1] = 0;
         write(tube[1], last_message, last_send);
+        free(last_message);
+        }
         close(tube[0]);
         close(tube[1]);
     }
